c4e3ececfbb2a87aa79cf47909c1f7f9506976a5
Ernesto Cruz Olivera <ecruzolivera@gmail.com>
2025-09-18 17:06:18 -0600

    atomic 16 has some issues it seems ??

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@
 #![feature(abi_avr_interrupt)]
 
 use arduino_hal::simple_pwm::{IntoPwmPin, Prescaler, Timer0Pwm};
-use core::sync::atomic::{AtomicU8, Ordering};
+use core::sync::atomic::{AtomicU16, Ordering};
 use embedded_hal::pwm::SetDutyCycle;
 use panic_halt as _;
 use ufmt::uwriteln;
 
 mod pseudo_rand;
 
-const HOUR: u16 = 3600;
-const HALF_HOUR: u16 = HOUR / 2;
+const MINUTE: u16 = 60;
+const T30: u16 = MINUTE * 30;
+const T45: u16 = MINUTE * 45;
+const T50: u16 = MINUTE * 50;
+
 const TIMER1_PRELOAD: u16 = 49911; // Preload for 1s overflow with prescaler 1024
-static SECONDS: AtomicU8 = AtomicU8::new(0);
 
 #[avr_device::interrupt(atmega2560)]
 fn TIMER1_OVF() {
-    let secs = SECONDS.load(Ordering::SeqCst);
-    SECONDS.store(secs + 1, Ordering::SeqCst);
     unsafe {
         // SAFETY: Accessing hardware register from ISR
         (*avr_device::atmega2560::TC1::ptr())
@@ -61,9 +61,13 @@ fn main() -> ! {
         avr_device::interrupt::enable();
     }
     let mut rng = pseudo_rand::XorShift8::new(69);
+    let mut off = false;
+    let mut seconds = 0;
     loop {
-        let seconds = SECONDS.load(Ordering::SeqCst);
+        let is_over_fifty = seconds >= T50;
+
         let delta = rng.random_between(-5, 5);
+        off = is_over_fifty && delta < 0;
         let duty_cycle = flick_torch(seconds, delta);
         led.set_duty_cycle_percent(duty_cycle).unwrap();
         uwriteln!(
@@ -74,25 +78,24 @@ fn main() -> ! {
             delta
         )
         .unwrap();
-        avr_device::asm::sleep();
-        if seconds > 60 {
+
+        if off {
             avr_device::interrupt::disable();
             led.set_duty_cycle_percent(0).unwrap();
             avr_device::asm::sleep();
         }
+        avr_device::asm::sleep();
+        seconds += 1;
     }
 }
 
-fn flick_torch(seconds: u8, delta: i8) -> u8 {
+fn flick_torch(seconds: u16, delta: i8) -> u8 {
+    const T31: u16 = T30 + 1;
+
     let adjusted_by_time: u8 = match seconds {
-        0..=30 => 90,
-        31..=45 => 60,
-        46..=59 => 40,
-        _ => 0,
+        0..=T30 => 90,
+        T31..=T45 => 50,
+        _ => 30,
     };
-    let flicked = adjusted_by_time.saturating_add_signed(delta);
-    match flicked {
-        v if v > 100 => 100,
-        _ => flicked,
-    }
+    adjusted_by_time.saturating_add_signed(delta)
 }
